13c48ebcd7f53362fe5a038bad4938c1b5720733
NataliSereda <natali.sereda.02@mail.ru>
2021-04-21 14:35:10 +0000

    first commit

---
+++ js1/main1.js
@@ -0,0 +1,32 @@
+import {GameView, clickToTail, canvas} from "./gameview1.js"
+import {move, goalTest,actions, successor} from "./game1.js"
+import {search} from "./search.js"
+const initialState = [
+    [4, 1, 0],
+    [7, 5, 3],
+    [8, 2, 6]]
+
+let s1 = initialState
+new GameView(s1)
+
+canvas.addEventListener('click', moveView)
+const btn_start_ai = document.querySelector('#btn_start_ai')
+btn_start_ai.addEventListener('click', start_ai)
+
+function moveView(event)
+{
+    const [i, j] = clickToTail(event.offsetX, event.offsetY)
+    s1 = move(s1, i, j)
+    new GameView(s1)
+}
+
+function start_ai()
+{
+    const solution = search(s1, goalTest, actions, successor, false)
+    for(let i = 0; i < solution.length; i++)
+    {
+        setTimeout(()=>{new GameView(solution[i] .state)}, i * 2000)
+        if(i === solution.length - 1)
+        s1 = solution[i].state
+    }
+}
